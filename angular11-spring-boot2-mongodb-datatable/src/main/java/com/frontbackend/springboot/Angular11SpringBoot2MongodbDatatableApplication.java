b6fcf3e7ba4660f0e2b34ed7e3f5b136c6085e29
Harry2915 <harishhibare2915@gmal.com>
2021-10-13 15:39:17 +0530

    final commit

---
+++ angular11-spring-boot2-mongodb-datatable/src/main/java/com/frontbackend/springboot/Angular11SpringBoot2MongodbDatatableApplication.java
@@ -0,0 +1,13 @@
+package com.frontbackend.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Angular11SpringBoot2MongodbDatatableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Angular11SpringBoot2MongodbDatatableApplication.class, args);
+	}
+
+}
---
+++ angular11-spring-boot2-mongodb-datatable/src/test/java/com/frontbackend/springboot/Angular11SpringBoot2MongodbDatatableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frontbackend.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Angular11SpringBoot2MongodbDatatableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
